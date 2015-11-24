03fe4568563417bf74f2f5b7b0d7a6beef02c20a
Pavel Podkorytov <pod.pavel@gmail.com>
2015-11-24 15:09:12 +0500

    rsyslogd/syslog-send.py: more verbose output

--- rsyslogd/syslog-send.py
+++ rsyslogd/syslog-send.py
@@ -11,20 +11,33 @@ Send 2 test message to remote syslog server (localhost:514/udp)
 import logging
 import logging.handlers
 
+HOSTNAME = 'localhost'
+PORT = 514
+
 rootLogger = logging.getLogger('')
 rootLogger.setLevel(logging.DEBUG)
-socketHandler = logging.handlers.SysLogHandler(address=('localhost', 514))
+socketHandler = logging.handlers.SysLogHandler(address=(HOSTNAME, PORT))
 rootLogger.addHandler(socketHandler)
 
+###
 config_rfc3164 = {
     'DATE': 'Oct 11 22:14:15',
     'HOSTNAME': 'mymachine',
     'TAG': 'programname[321]:',
     'MSG': 'rfc3164 test logger message',
     }
+template_rfc3164 = '{DATE} {HOSTNAME} {TAG} {MSG}'
+message_rfc3154 = template_rfc3164.format(**config_rfc3164)
 
-logging.info('{DATE} {HOSTNAME} {TAG} {MSG}'.format(**config_rfc3164))
+print('rfc3164:')
+for k, v in config_rfc3164.items():
+    print('  {0:10} {1}'.format(k, v))
+print('  {0:10} {1}'.format('Template', template_rfc3164))
+print('  {0:10} {1}'.format('Message', message_rfc3154))
+logging.info(message_rfc3154)
 
+
+###
 config_rfc5424 = {
     'VERSION': '1',
     'DATE': '1985-04-12T23:20:50.52Z',
@@ -35,4 +48,12 @@ config_rfc5424 = {
     'MSG': 'rfc5424 test logger message в юникоде'
     }
 
-logging.info('{VERSION} {DATE} {HOSTNAME} {APP-NAME} {PROCID} {MSGID} {MSG}'.format(**config_rfc5424))
+template_rfc5242 = '{VERSION} {DATE} {HOSTNAME} {APP-NAME} {PROCID} {MSGID} {MSG}'
+message_rfc5242 = template_rfc5242.format(**config_rfc5424)
+
+print('\nrfc5242:')
+for k, v in config_rfc5424.items():
+    print('  {0:10} {1}'.format(k, v))
+print('  {0:10} {1}'.format('Template', template_rfc5242))
+print('  {0:10} {1}'.format('Message', message_rfc5242))
+logging.info(message_rfc5242)
